efb7f339e7a9f2b04a4101565f0ee6a7c20c78e4
Dheeraj Aldak <144196534+Dheerajaldak@users.noreply.github.com>
2025-05-10 19:38:49 +0530

    Update webRTCHandler.js

--- client/src/realtimeCommunication/webRTCHandler.js
+++ client/src/realtimeCommunication/webRTCHandler.js
@@ -25,7 +25,7 @@ export const getAccessToLocalStream = async () => {
 
 export const connectWithPeerServer = () => {
   peer = new Peer(undefined, {
-    host: "localhost",
+    host: "https://salestrackerversion1visyscloudtechdheeraja.vercel.app",
     port: 9000,
     path: "/peer",
      secure: false,
